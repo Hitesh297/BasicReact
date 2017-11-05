ad1b852ddaac8f46b49a6e8f9a64e34cc7ac1db2
VAIO\Hitesh <hites.297@gmail.com>
2017-11-05 13:19:36 +0530

    api call only
    Does api call and console log it

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LaunchButton from './components/launch';
 import { getAllSurveys } from './actions/getSurveys_action';
-import SurveyList from './components/survey_list';
 
+var list;
 var test = getAllSurveys();
 test.then(function(res){
  console.log(res.data);
+ list = res.data;
 });
 
 const App = ()=>{
   return (
     <div>
-      <LaunchButton />
-      <SurveyList />
+    {list}
    </div>
  );
 }
